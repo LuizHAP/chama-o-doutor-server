02f57aef42c5a18f0b16a4ed2b3824abef98be2a
Luiz Pansarini <luiz.pansarini@uaubox.com.br>
2020-10-19 19:19:04 -0300

    answers add try catch and return 400 with error

--- src/controllers/AnswersController.ts
+++ src/controllers/AnswersController.ts
@@ -3,11 +3,18 @@ import db from '../database/connection';
 
 export default class AnswersController {
     async create(req: Request, res: Response) {
-        const { id_quiz, nomeColaborador, candPrefeito, governoKiko, candPrefKiko, querConhecer } = req.body;
+        try {
+            const { id_quiz, nomeColaborador, candPrefeito, governoKiko, candPrefKiko, querConhecer } = req.body;
 
-        await db('answers').insert({
-            id_quiz, nomeColaborador, candPrefeito, governoKiko, candPrefKiko, querConhecer
-        })
+            await db('answers').insert({
+                id_quiz, nomeColaborador, candPrefeito, governoKiko, candPrefKiko, querConhecer
+            })
+        } catch (e) {
+            console.log(e);
+            return res.status(400).json({
+                error: 'Unexpected error while creating new class'
+            })
+        }
 
         return res.status(201).send();
     }
